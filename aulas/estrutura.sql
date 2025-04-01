17ef8456098adefccd47be11ecc1c14db05f2faa
GuilhermeSerafim <129989701+GuilhermeSerafim@users.noreply.github.com>
2025-04-01 11:54:59 +0000

    Entendendo a estrutura do sql

---
+++ aulas/estrutura.sql
@@ -0,0 +1,7 @@
+SELECT       -- o que você quer ver
+FROM         -- de qual tabela
+WHERE        -- filtros linha a linha (antes do agrupamento)
+GROUP BY     -- agrupamento (ex: por cliente, por produto...)
+HAVING       -- filtro dos grupos (após agrupamento)
+ORDER BY     -- ordenação do resultado final
+LIMIT        -- limita a quantidade de linhas (opcional)
